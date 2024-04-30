113cd8e8c1f5f8cb84bfd7eead0e7cb93e2af140
Taylor Bergeron <tberg1234@gmail.com>
2024-04-30 15:20:25 -0400

    additional results

--- evaluation/evaluate_images.py
+++ evaluation/evaluate_images.py
@@ -12,14 +12,20 @@ def create_results(TYPE):
 
     indices = ["{0:03}".format(i) for i in range(120)]
 
+    with open('csvs/'+TYPE+'_nonwhite_pixel_amt.csv', 'a', newline='') as csvfile:
+            csvfile.write('\n google-vison, grounding-dino')
+
     for i in indices:
         img1 = cv.imread('results/'+TYPE+'/google-vision/flythrough/seged_img/'+i+'.png')
         img2 = cv.imread('results/'+TYPE+'/grounding-dino/flythrough/seged_img/'+i+'.png')
 
         img1_gray = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
+
         img2_gray = cv.cvtColor(img2, cv.COLOR_BGR2GRAY)
 
         mse = mean_squared_error(img1_gray, img2_gray)
+
+        bitwise_xor = cv.bitwise_xor(img2_gray, img1_gray)
 
         print("MSE value:", mse)        
 
@@ -27,8 +33,16 @@ def create_results(TYPE):
 
         cv.imwrite("mse_images/"+TYPE+"/"+i+".png", diff)
 
+        cv.imwrite("bitwise_xor_images/"+TYPE+"/"+i+".png", bitwise_xor)
+
+        img1_sum = len(img1_gray[img1_gray != 255])
+        img2_sum = len(img2_gray[img2_gray != 255])
+
         with open('csvs/'+TYPE+'_mse.csv', 'a', newline='') as csvfile:
             csvfile.write('\n'+str(mse))
+
+        with open('csvs/'+TYPE+'_nonwhite_pixel_amt.csv', 'a', newline='') as csvfile:
+            csvfile.write('\n'+str(img1_sum)+","+str(img2_sum))
 
 if __name__ == "__main__":
     TYPE="flower"
